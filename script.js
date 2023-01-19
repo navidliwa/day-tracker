1301ee2cff03c1ce46f04eee5937acdab4730404
awildivan <75466291+navidliwa@users.noreply.github.com>
2023-01-18 22:28:27 -0700

    deleted 'to do' comments and added more comments. made small changes to js

--- script.js
+++ script.js
@@ -2,60 +2,48 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  $(".saveBtn").on("click", function () {
 
-    // Get nearby values of the description in JQuery
-    var text = $(this).siblings(".description").val();
-    var time = $(this).parent().attr("id");
+  // Add click event to saveBtn class
+  $(".saveBtn").click(function (e) {
 
-    // Save text in local storage
-    localStorage.setItem(time, text);
-  })
+    e.preventDefault();
 
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+    // Get the textarea value and id of the time-block
+    var note = $(this).parent().children("textarea").val();
+    var hour = $(this).parent().attr("id");
+
+    // Save text and time in local storage
+    localStorage.setItem(hour, note);
+  });
+
   // Creates function to add and remove classes from time blocks
   function handleTimeblocks() {
     // Get current hour
-    var timeNow = dayjs().hour();
+    var currentTime = dayjs().hour();
     
     // Loop over time blocks
     $(".time-block").each(function() {
       // Parse the number portion of a time block's id into an integer
-      var blockTime = parseInt($(this).attr("id").split("hour-")[1]);
+      var divTime = parseInt($(this).attr("id").split("-")[1]);
 
-      // Check the time to add or remove classes
-      if (blockTime < timeNow) {
+      // Compare currentTime and divTime to add or remove classes
+      if (divTime === currentTime) {
         $(this).removeClass("future");
-                $(this).removeClass("present");
-                $(this).addClass("past");
-            }
-            else if (blockTime === timeNow) {
-                $(this).removeClass("past");
-                $(this).removeClass("future");
-                $(this).addClass("present");
-            }
-            else {
-                $(this).removeClass("present");
-                $(this).removeClass("past");
-                $(this).addClass("future");
+        $(this).removeClass("past");
+        $(this).addClass("present");
+      } else if (divTime > currentTime) {
+        $(this).removeClass("past");
+        $(this).removeClass("present");
+        $(this).addClass("future");
+      } else {
+        $(this).removeClass("present");
+        $(this).removeClass("future");
+        $(this).addClass("past");
       }
     })
-  }
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
+  };
+  
+  // Gets items from local storage and puts them in corresponding time-blocks
   $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   $("#hour-10 .description").val(localStorage.getItem("hour-10"));
   $("#hour-11 .description").val(localStorage.getItem("hour-11"));
